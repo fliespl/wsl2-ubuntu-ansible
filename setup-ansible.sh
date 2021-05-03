4d7d8d9bcd0ca4f679e36305bffe5b310d493c1b
flies <a.rzadkowolski@gmail.com>
2021-05-03 15:16:47 +0200

    ubuntu 21.04 support

--- setup-ansible.sh
+++ setup-ansible.sh
@@ -7,7 +7,7 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 93C4A3FD7BB9C367
 if [ "$(awk -F= '/^ID=/{print $2}' /etc/os-release)" = "debian" ]; then
   # for debian we use trusty
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/ansible.list
-else
+elif [ "$(awk -F= '/^UBUNTU_CODENAME=/{print $2}' /etc/os-release)" = "focal" ]; then
   echo "deb http://ppa.launchpad.net/ansible/ansible/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/ansible.list
 fi
 
